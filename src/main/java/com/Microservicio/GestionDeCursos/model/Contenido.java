82bb4ee631ac518f5c67d5d326282b4d0f858941
Matthew <ma.lepillan@duocuc.cl>
2025-05-17 22:36:13 -0400

    Nuevo commit

--- src/main/java/com/Microservicio/GestionDeCursos/model/Contenido.java
+++ src/main/java/com/Microservicio/GestionDeCursos/model/Contenido.java
@@ -1,11 +1,10 @@
 package com.Microservicio.GestionDeCursos.model;
 
-import java.util.List;
+
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
