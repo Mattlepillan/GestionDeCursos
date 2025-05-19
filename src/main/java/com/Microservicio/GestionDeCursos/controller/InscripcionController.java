160e468a982e307cbdc7181f5c5afe1162dbf06e
Matthew <ma.lepillan@duocuc.cl>
2025-05-18 22:29:11 -0400

    Nuevo commit

--- src/main/java/com/Microservicio/GestionDeCursos/controller/InscripcionController.java
+++ src/main/java/com/Microservicio/GestionDeCursos/controller/InscripcionController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,8 +55,19 @@ public class InscripcionController {
         }
         return new ResponseEntity<>(HttpStatus.CREATED);
         
-
-
     }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Curso> deleteInscripcion(@PathVariable int id, @RequestBody Inscripcion inscripcion){
+        Inscripcion buscado = inscripcionService.findById(inscripcion.getId());
+        if(buscado == null)
+        {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else
+        {
+            inscripcionService.delete(inscripcion);
+            return new ResponseEntity<>( HttpStatus.OK);
+        }
+    }
 }
